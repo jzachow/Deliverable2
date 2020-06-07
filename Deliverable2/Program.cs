49dfb17c83fe17f47e397caec2b70d9e06929b90
Justin <justin.zachow@gmail.com>
2020-06-07 12:58:02 -0400

    First commit for Grand Circus deliverable 2 project

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string message = "";
+            int checksum = 0;
+
+            Console.WriteLine("What is your message?");
+            input = Console.ReadLine();
+            input = input.ToUpper();
+
+
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                int uni = input[i];
+                checksum += uni;
+
+                uni -= 64;
+                string temp = uni.ToString();
+                message += temp;
+
+                if (!(i == input.Length - 1))
+                {
+                    message += "-";
+                }
+
+
+            }
+           
+            Console.WriteLine("\nYour encoded message is: " + message);
+            Console.WriteLine("Message checksum is: " + checksum);
+        }
+    }
+}
